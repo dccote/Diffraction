935ab22ae458b7ad7d85de26f318c05715bb2433
Daniel Côté <dccote@novajo.ca>
2018-03-28 03:13:41 -0400

    Premiers tests de generation d'images
    Debut de refactorisation, nouveau code de generation d'images carrées et circulaires.

--- TestDiffraction.m
+++ TestDiffraction.m
@@ -1,14 +1,51 @@
-%% Test Class Definition
 classdef TestDiffraction < matlab.unittest.TestCase
-    
-    %% Test Method Block
     methods (Test)
         
-        %% Test Function
         function testATest(testCase)      
-            %% Exercise function under test
-            % act = the value from the function under test
             testCase.verifyEqual(2+3,5);
         end
+    
+        function testWhatIsAMesh(testCase)
+            [x,y]=meshgrid(-2:1:2, -2:1:2);
+            testCase.verifyEqual([ -2 -1 0 1 2; -2 -1 0 1 2; -2 -1 0 1 2; -2 -1 0 1 2;-2 -1 0 1 2 ], x);
+            testCase.verifyEqual([ -2 -1 0 1 2; -2 -1 0 1 2; -2 -1 0 1 2; -2 -1 0 1 2;-2 -1 0 1 2 ]', y);
+        end
+        
+        function testSquareGeneration(testCase)      
+            [x,y]=meshgrid(-2:1:2, -2:1:2);
+            image = generateCenteredSquare(x,y, 1);
+            
+            testCase.verifyEqual(logical([ 0 0 0 0 0;0 0 0 0 0; 0 0 1 0 0; 0 0 0 0 0; 0 0 0 0 0]), image);
+        end
+        
+        function testBigSquareGeneration(testCase)      
+            [x,y]=meshgrid(-2:1:2, -2:1:2);
+            image = generateCenteredSquare(x,y, 3);
+            
+            testCase.verifyEqual(logical([ 0 0 0 0 0;0 1 1 1 0; 0 1 1 1 0; 0 1 1 1 0; 0 0 0 0 0]), image);
+        end
+        
+        
+        function testCircleGeneration(testCase)      
+            [x,y]=meshgrid(-2:1:2, -2:1:2);
+            image = generateCenteredCircle(x,y, 1);
+            
+            testCase.verifyEqual(logical([ 0 0 0 0 0;0 0 0 0 0; 0 0 1 0 0; 0 0 0 0 0; 0 0 0 0 0]), image);
+        end
+        
+        function testBigCircleGeneration(testCase)      
+            [x,y]=meshgrid(-2:1:2, -2:1:2);
+            image = generateCenteredCircle(x,y, 2);
+            
+            testCase.verifyEqual(logical([ 0 0 0 0 0;0 1 1 1 0; 0 1 1 1 0; 0 1 1 1 0; 0 0 0 0 0]), image);
+        end
+        
+        function testVeryBigCircleGeneration(testCase)
+            [x,y]=meshgrid(-3:1:3, -3:1:3);
+            image = generateCenteredCircle(x,y, 2.5);
+            
+            testCase.verifyEqual(logical([ 0 0 0 0 0 0 0; 0 0 1 1 1 0 0 ; 0 1 1 1 1 1 0; 0 1 1 1 1 1 0; 0 1 1 1 1 1 0; 0 0 1 1 1 0 0; 0 0 0 0 0 0 0]), image);
+        end
+    
     end
 end
