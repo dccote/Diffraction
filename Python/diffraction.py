aa7e0811985ea58cb903159ac44ee0f2f0ad6073
Daniel Cote <dccote@novajo.ca>
2019-03-29 07:56:58 -0400

    Update wirth grating option

--- Python/diffraction.py
+++ Python/diffraction.py
@@ -13,7 +13,8 @@ def diffraction(sCoords, A, k, r, yCoords):
     on the screen at yCoords. Done. """
 
     Efield=[0]*Ny # Initialize with no field for a given r
-    for ys in sCoords:
+    for j,ys in enumerate(sCoords):
+        reportProgress(j,len(sCoords),"Summing source")
         for i,yf in enumerate(yCoords):
             R = sqrt( (yf-ys)*(yf-ys)+r*r)
             # we don't divide by r because we keep everything normalized
@@ -24,17 +25,17 @@ def diffraction(sCoords, A, k, r, yCoords):
 def createFigure():
     fig, axes = plt.subplots(figsize=(10, 7))
     axes.set(xlabel='Distance', ylabel='Intensite', title="Profil de Diffraction")
-    axes.set_ylim([0,1])
+#    axes.set_ylim([0,1])
     return fig, axes
 
 
-def reportProgress(r, Nr):
+def reportProgress(r, Nr, label=""):
     tenPercent = int(Nr/10)
     if tenPercent == 0:
         tenPercent = 1
 
     if r % tenPercent == 0:
-        print("Progress {0:.0f}%".format(100*r/Nr))
+        print("{0} {1:.0f}%".format(label, 100*r/Nr))
 
 if __name__ == "__main__":
       # This code computes the diffraction pattern in the simplest way possible:
@@ -43,21 +44,31 @@ if __name__ == "__main__":
 
     # Distance to screen
     R = 30000 # Distance source-screen in µm
-    Nr = 2
+    Nr = 1
     dR = R/Nr
     rCoords = [(r+1)*dR for r in range(Nr)]
 
     # Screen size in µm
-    Y = 1000
-    dy = 1
+    Y = 60000
+    dy = 100
     Ny = int(Y/dy)
     yCoords = [(j-Ny/2)*dy for j in range(Ny)]
 
     # Source size in µm
-    S = 50
-    ds = 0.1
+    S = 4
+    ds = 0.01
     Ns = int(S/ds)
     sCoords = [(j-Ns/2)*ds for j in range(Ns)]
+    
+    # Grating rep
+    da = 10
+    Na = 1x
+    aCoords = [(j-Na/2)*da for j in range(Na)]
+
+    gCoords = []
+    for a in aCoords:
+        gCoords.extend([ s+a for s in sCoords])
+
     amplitude = (1/float(Ns)) # normalized
 
     wavelength = 1
@@ -66,9 +77,11 @@ if __name__ == "__main__":
     for idx,r in enumerate(rCoords): 
         reportProgress(idx,Nr)
 
+        Gfield = diffraction(gCoords, amplitude/Na, k, r, yCoords)
         Efield = diffraction(sCoords, amplitude, k, r, yCoords)
         
         fig, axes = createFigure()
+        axes.plot(yCoords, abs(Gfield*conjugate(Gfield)), linewidth=1)
         axes.plot(yCoords, abs(Efield*conjugate(Efield)), linewidth=1)
         plt.show()
 
