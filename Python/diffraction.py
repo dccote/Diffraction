89e2e7abe3010855ccb797065903ff021577832b
Daniel Cote <dccote@novajo.ca>
2019-04-03 03:00:12 -0400

    Commentaire sur les graphiques.

--- Python/diffraction.py
+++ Python/diffraction.py
@@ -32,7 +32,7 @@ def createFigure():
     axes.set_title("Profil de Diffraction")
     return fig, axes
 
-def showDiffractedIntensity(screenCoordinates, Efield, envelope=None, title=""):
+def showDiffractedIntensity(screenCoordinates, Efield, envelope=None, title="", comment=""):
     fig, axes = createFigure()
 
     axes.plot(screenCoordinates, abs(Efield*conjugate(Efield)), linewidth=1,label="Diffracted beam")
@@ -42,6 +42,7 @@ def showDiffractedIntensity(screenCoordinates, Efield, envelope=None, title=""):
 
     axes.set(xlabel='Position [µm]', ylabel='Intensite')
     axes.set_title(title, ha='center',size=13)
+    axes.text(0.05,0.95, comment, size=10, va='top', bbox=dict(boxstyle="square", fc='w',ec='k', lw=1,ls=":"), transform=axes.transAxes)
     plt.show()
 
 
@@ -126,7 +127,9 @@ if __name__ == "__main__":
     Nb = 2
     (gratingCoords, gratingAmplitudes) = sourcePeriodicSlitOpaqueWall(aCoords, amplitudes, b=b, Nb=Nb)
     Efield = diffraction(gratingCoords, gratingAmplitudes, k, R, screenCoords, progressMessage="Calcul fente double")
-    showDiffractedIntensity( screenCoords, Efield, envelope=envelope, title="Figure: Profil à R={0} mm de 2 fentes de largeur a=5 µm séparées par {1:d} µm".format(R/1000, b-a))
+    showDiffractedIntensity( screenCoords, Efield, envelope=envelope, 
+        title="Figure: Profil à R={0} mm de 2 fentes de largeur a=5 µm séparées par {1:d} µm".format(R/1000, b-a),
+        comment="Notez l'interference entre les {0} fentes\net l'enveloppe générale correspondant\nà une seule fente".format(Nb))
 
     # Third example: 10 slits separated by small distance (b-a)
     (aCoords, amplitudes) = sourceSingleSlitOpaqueWall(a=a, da=da)
@@ -134,7 +137,9 @@ if __name__ == "__main__":
     Nb = 10
     (gratingCoords, gratingAmplitudes) = sourcePeriodicSlitOpaqueWall(aCoords, amplitudes, b=b, Nb=Nb)
     Efield = diffraction(gratingCoords, gratingAmplitudes, k, R, screenCoords, progressMessage="Calcul {0} fentes".format(Nb))
-    showDiffractedIntensity(screenCoords, Efield, envelope=envelope, title="Figure: Profil à R={0} mm de 10 fentes de largeur a=5 µm séparées par {1:d} µm".format(R/1000, b-a))
+    showDiffractedIntensity(screenCoords, Efield, envelope=envelope, 
+        title="Figure: Profil à R={0} mm de 10 fentes de largeur a=5 µm séparées par {1:d} µm".format(R/1000, b-a),
+        comment="Notez l'interference plus mince entre\nles {0} fentes encore sous l'enveloppe\ngénérale correspondant à une\nseule fente".format(Nb))
 
     # Fourth example: single slit with a linear phase mask from minPhase to maxPhase
     d = 0.2
@@ -143,25 +148,31 @@ if __name__ == "__main__":
     maxPhase=k*d*n
     (aCoords, amplitudes) = sourceSingleSlitWithLinearPhaseOpaqueWall(a=a, da=da, minPhase=minPhase, maxPhase=maxPhase)
     Efield = diffraction(aCoords, amplitudes, k, R, screenCoords, progressMessage="Calcul fente simple avec masque")
-    showDiffractedIntensity(screenCoords, Efield,title="Figure: Profil à R={0} mm d'une fente de largeur a=5 µm\navec un masque de phase lineaire de {1:0.2f} rad à {2:0.2f} rad\nNotez le deplacement vers la droite.".format(R/1000, minPhase, maxPhase))
+    showDiffractedIntensity(screenCoords, Efield,
+        title="Figure: Profil à R={0} mm d'une fente de largeur a=5 µm\navec un masque de phase lineaire de {1:0.2f} rad à {2:0.2f} rad".format(R/1000, minPhase, maxPhase),
+        comment="Notez le déplacement vers la droite\ndu patron de diffraction, dû à la phase\nlinéaire sur la fente")
 
     # Fifth example: grating of slits from #4 with a linear phase mask from minPhase to maxPhase
     (aCoords, amplitudes) = sourceSingleSlitWithLinearPhaseOpaqueWall(a=a, da=da, minPhase=minPhase, maxPhase=maxPhase)
     (gratingCoords, gratingAmplitudes) = sourcePeriodicSlitOpaqueWall(aCoords, amplitudes, b=10, Nb=5)
     envelope = abs(Efield*conjugate(Efield)) # The field from example #4 gives us the envelope
     Efield = diffraction(gratingCoords, gratingAmplitudes, k, R, screenCoords, progressMessage="Calcul reseau de fentes simples avec masque")
-    showDiffractedIntensity(screenCoords, Efield, envelope=envelope, title="Figure: Profil à R={0} mm de 10 fentes de largeur a=5 µm\navec un masque de phase lineaire de {1:0.2f} rad à {2:0.2f} rad".format(R/1000, minPhase, maxPhase))
+    showDiffractedIntensity(screenCoords, Efield, envelope=envelope, 
+        title="Figure: Profil à R={0} mm de 10 fentes de largeur a=5 µm\navec un masque de phase lineaire de {1:0.2f} rad à {2:0.2f} rad".format(R/1000, minPhase, maxPhase),
+        comment="Notez le déplacement vers la droite\ndu patron de diffraction, dû à la phase\nlinéaire sur chaque fente. Maintenant,\nl'ordre 1 est maximum plutôt que l'ordre 0.")
 
     # Sixth example: single slit, but in Fresnel zone (very close!)
     (aCoords, amplitudes) = sourceSingleSlitOpaqueWall(a, da)
     Rclose = 10
     screenCoordsClose = [(j-(Ny-1)/2)*dY/200 for j in range(Ny)]
     Efield = diffraction(aCoords, amplitudes, k, Rclose, screenCoordsClose, progressMessage="Calcul fente simple, zone de Fresnel")
-    showDiffractedIntensity(screenCoordsClose, Efield,title="Figure: Profil dans le regime de Fresnel à R={0} µm d'une fente de largeur a={1} µm".format(Rclose,a))
+    showDiffractedIntensity(screenCoordsClose, Efield,
+        title="Figure: Profil dans le regime de Fresnel à R={0} µm d'une fente de largeur a={1} µm".format(Rclose,a),
+        comment="Le patron de diffraction d'une fente\nrectangulaire n'est pas un sinc(x)\ndans le champ proche.")
 
     # Seventh example: single slit diffraction regime from fresnel (very close) to fraunhofer (very far)
     rCoords = [2,5,10,20,50,100]
-    screenCoordsClose = [(j-Ny/2)*dY/30 for j in range(Ny)]
+    screenCoordsClose = [(j-Ny/2)*dY/60 for j in range(Ny)]
     fig, axes = createFigure()
     for idx,r in enumerate(rCoords):
         Efield = diffraction(aCoords, amplitudes, k, r, screenCoordsClose, progressMessage="Calculating single slit at distance r={0}".format(r))
