e4a5f6a526e389c2cb48665ee13b869b0fc991b2
Daniel Côté <dccote@novajo.ca>
2020-03-20 23:56:46 -0400

    diffraction2D.py
    Seulement la version de la fente arbitraire, et aucune interaction GUI: tout est directement dans le code.

--- Python/diffraction2D.py
+++ Python/diffraction2D.py
@@ -1,20 +1,21 @@
 from PIL import Image
 import numpy as np
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
+from matplotlib.widgets import Slider, Button, RadioButtons
 
 # Beaucoup d'information sur la gestion d'image pour calculs en Python ici
 # https://www.pluralsight.com/guides/importing-image-data-into-numpy-arrays
 
-# load image as pixel array
-image = Image.open("/tmp/test.png")
-# summarize shape of the pixel array
-# display the array of pixels as an image
-pyplot.imshow(image)
-pyplot.show()
+image = Image.open("arbitraire.tif")
+imageData = np.asarray(image,dtype=np.float64)    # Convertit en point-flottant
+imageData = np.fft.fft2(imageData)                # Transformee de Fourier 2D
+imageData = np.abs(np.fft.fftshift(imageData))**2 # Puissance reelle
 
-imageData = np.asarray(image)
-imageData = np.fft.fft2(imageData)
-imageData = np.fft.fftshift(imageData)
+normalization = np.max(imageData)                 # Pour normaliser
+intensityFactor = 10000                           # Pour voir les ailes
 
-pyplot.imshow(Image.fromarray(imageData))
-pyplot.show()
+imageData = imageData/normalization*intensityFactor
+plt.imshow(Image.fromarray(imageData))
+plt.show()
+
+
